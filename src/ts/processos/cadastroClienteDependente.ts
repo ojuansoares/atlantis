8e53cd82eb8760276805bf627c3e6a7732aafd7d
Juan Soares <juan.garcia.soares@gmail.com>
2024-10-05 10:55:06 -0300

    iniciando o cadastro de cliente dependente

---
+++ src/ts/processos/cadastroClienteDependente.ts
@@ -0,0 +1,7 @@
+import Processo from "../abstracoes/processo";
+
+export default class CadastroClienteDependente extends Processo {
+    processar(): void {
+        console.log('Iniciando o cadastro de um novo cliente dependente...')
+    }
+}
