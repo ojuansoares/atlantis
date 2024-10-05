786513846f7935e30245b67f14b142b2fa7da714
Juan Soares <juan.garcia.soares@gmail.com>
2024-10-05 10:55:38 -0300

    iniciando a listagem de dependentes por titular

--- src/ts/dominio/armazem.ts
+++ src/ts/dominio/armazem.ts
@@ -10,5 +10,4 @@ export default class Armazem {
     public get Clientes() {
         return this.clientes
     }
-
 }
---
+++ src/ts/processos/listagemDependentesTitular.ts
@@ -0,0 +1,15 @@
+import Processo from "../abstracoes/processo";
+import Armazem from "../dominio/armazem";
+import Cliente from "../modelos/cliente";
+
+export default class ListagemTitulares extends Processo {
+    private clientes: Cliente[]
+    constructor() {
+        super()
+        this.clientes = Armazem.InstanciaUnica.Clientes
+    }
+    processar(): void {
+        console.clear()
+        console.log('Iniciando a listagem dos clientes titulares...')
+    }
+}
