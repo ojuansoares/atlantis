bc018862a7ad2c3679f55ad78ea98214768e817f
Juan Garcia Soares <juan.garcia.soares@gmail.com>
2024-10-10 09:41:38 -0300

    implementação da edição do cliente Titular

--- src/ts/processos/edicoes/edicaoClienteTitular.ts
+++ src/ts/processos/edicoes/edicaoClienteTitular.ts
@@ -2,39 +2,98 @@ import Processo from "../../abstracoes/processo";
 import Armazem from "../../dominio/armazem";
 import Cliente from "../../modelos/cliente";
 import ListagemEdicaoTitular from "../listagens/listagemEdicaoTitular";
+import ListagemEdicaoDocumento from "../listagens/listagemEdicaoDocumento";
+import EdicaoEnderecoTitular from "./edicaoEnderecoTitular";
 
 export default class EdicaoClienteTitular extends Processo {
     processar(): void {
+
+        const quantidadeClientes = Armazem.InstanciaUnica.Clientes.length
+        if (quantidadeClientes === 0) {
+            console.log('Não há clientes para editar.');
+            return;
+        }
 
         this.processo = new ListagemEdicaoTitular()
         this.processo.processar()
 
         let sub = true
-        let clienteIndex: number = -1;
+        let clienteCPF: string = '';
+        let clienteEditar: Cliente = new Cliente('', '', new Date());
         while (sub) {
-            clienteIndex = this.entrada.receberNumero('Qual Cliente Titular Deseja Editar?')
-            if (clienteIndex < Armazem.InstanciaUnica.Clientes.length) {
-                sub = false
+            clienteCPF = this.entrada.receberTexto('CPF do Cliente Titular Que Deseja Editar: ')
+        
+            const clienteEncontrado = Armazem.InstanciaUnica.Clientes.find(cliente => 
+                cliente.Documentos.some(doc => doc.Tipo === 'Cadastro de Pessoas Física' && doc.Numero === clienteCPF) && 
+                cliente.Titular === undefined
+            );
+
+            if (clienteEncontrado) {
+                clienteEditar = clienteEncontrado;
+                sub = false;
+                break;
             } else {
-                console.log('Cliente não encontrado, tente novamente.')
+                console.log('Cliente não encontrado');
+                return;
             }
         }
 
-        let clienteEditar = Armazem.InstanciaUnica.Clientes[clienteIndex]
+        // Receber as novas informações do cliente e atualizar diretamente
+        let nome = this.entrada.receberTexto('Qual o novo nome do cliente?');
+        if (nome) {
+            clienteEditar.Nome = nome;
+        }
 
-        
+        let nomeSocial = this.entrada.receberTexto('Qual o novo nome social do cliente?');
+        if (nomeSocial) {
+            clienteEditar.NomeSocial = nomeSocial;
+        }
 
-        return
+        let dataNascimento = this.entrada.receberData('Qual a nova data de nascimento do cliente?');
+        if (dataNascimento) {
+            clienteEditar.DataNascimento = dataNascimento;
+        }
 
-        console.log('Iniciando Edição do Cliente Titular (Deixe em Branco para Não Editar).')
-        let nome = this.entrada.receberTexto('Qual o novo nome cliente?')
-        let nomeSocial = this.entrada.receberTexto('Qual o novo nome social do cliente?')
-        let dataNascimento = this.entrada.receberData('Qual a nova data de nascimento?')
-        let cliente = new Cliente(nome, nomeSocial, dataNascimento)
+        this.processo = new EdicaoEnderecoTitular(clienteEditar);
+        this.processo.processar();
 
-        let armazem = Armazem.InstanciaUnica
-        armazem.Clientes.push(cliente)
+        this.processo = new ListagemEdicaoDocumento(clienteEditar);
+        this.processo.processar();
 
-        console.log('Finalizando o cadastro do cliente...')
+        let sub2 = true;
+        while (sub2) {
+            let documentoIndexTexto = this.entrada.receberTexto('Qual Documento Deseja Editar?');
+            if (!documentoIndexTexto) {
+                sub2 = false;
+                break;
+            }
+
+            let documentoIndex = parseInt(documentoIndexTexto, 10);
+            if (!isNaN(documentoIndex) && documentoIndex >= 0 && documentoIndex < clienteEditar.Documentos.length) {
+                let novoNumero = this.entrada.receberTexto('Qual o novo número do documento?');
+                if (novoNumero) {
+                    clienteEditar.Documentos[documentoIndex].Numero = novoNumero;
+                }
+                let novaDataExpedicao = this.entrada.receberData('Qual a nova data de expedição do documento?');
+                if (novaDataExpedicao) {
+                    clienteEditar.Documentos[documentoIndex].DataExpedicao = novaDataExpedicao;
+                }
+
+                sub2 = false;
+            } else {
+                console.log('Documento não encontrado. Tente novamente.');
+            }
+        }
+
+        // Substituir o cliente existente pelo cliente atualizado no Armazem
+        const clienteIndex = Armazem.InstanciaUnica.Clientes.findIndex(cliente => 
+            cliente.Documentos.some(doc => doc.Tipo === 'Cadastro de Pessoas Física' && doc.Numero === clienteCPF)
+        );
+
+        if (clienteIndex !== -1) {
+            Armazem.InstanciaUnica.Clientes[clienteIndex] = clienteEditar;
+        }
+
+        console.log('Finalizando a edição do cliente...')
     }
 }
