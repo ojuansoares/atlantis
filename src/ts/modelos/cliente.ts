10f9ffbd225eb18616d630078bc96e601ed16129
Juan Garcia Soares <juan.garcia.soares@gmail.com>
2024-10-10 09:52:26 -0300

    implementação de dos 'public set' dentro dos modelos de cliente e documento

--- src/ts/modelos/cliente.ts
+++ src/ts/modelos/cliente.ts
@@ -30,5 +30,9 @@ export default class Cliente {
     public get Dependentes() { return this.dependentes }
     public get Titular() { return this.titular }
 
-    public set Endereco(endereco: Endereco) { this.endereco = endereco }
+    public set Nome(nome: string) { this.nome = nome; }
+    public set NomeSocial(nomeSocial: string) { this.nomeSocial = nomeSocial; }
+    public set DataNascimento(dataNascimento: Date) { this.dataNascimento = dataNascimento; }
+    public set Endereco(endereco: Endereco) { this.endereco = endereco; }
+    public set Documentos(documentos: Documento[]) { this.documentos = documentos; }
 }
