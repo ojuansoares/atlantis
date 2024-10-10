6c87c4b9b00fab51ae0960777058cbd6aa6ec224
Juan Garcia Soares <juan.garcia.soares@gmail.com>
2024-10-10 10:17:51 -0300

    implementação de segurança no cadastro de cada documento, deixando eles unicos por usuario e datas de expedição validas

--- src/ts/processos/documentos/cadastroCpf.ts
+++ src/ts/processos/documentos/cadastroCpf.ts
@@ -2,6 +2,7 @@ import Processo from "../../abstracoes/processo";
 import { TipoDocumento } from "../../enumeracoes/TipoDocumento";
 import Cliente from "../../modelos/cliente";
 import Documento from "../../modelos/documento";
+import Armazem from "../../dominio/armazem";
 
 export default class CadastroCpf extends Processo {
     private cliente: Cliente
@@ -11,8 +12,30 @@ export default class CadastroCpf extends Processo {
     }
 
     processar(): void {
-        let numero = this.entrada.receberTexto('Qual o número do documento?')
-        let dataExpedicao = this.entrada.receberData('Qual a data de expedição do documento?')
+
+        let numero: string
+        let cpfExistente: boolean
+
+        do {
+            numero = this.entrada.receberTexto('Qual o número do documento?')
+            cpfExistente = Armazem.InstanciaUnica.Clientes.some(cliente =>
+            cliente.Documentos.some(doc => doc.Tipo === TipoDocumento.CPF && doc.Numero === numero)
+            )
+            if (cpfExistente) {
+                console.log('CPF já existe. Por favor, insira um CPF diferente.')
+            } else if (isNaN(Number(numero))) {
+                console.log('CPF inválido. Por favor, insira um CPF composto apenas por números.')
+                cpfExistente = true
+            }
+        } while (cpfExistente || !numero)
+
+        let dataExpedicao: Date
+        do {
+            dataExpedicao = this.entrada.receberData('Qual a data de expedição do documento?')
+            if (isNaN(dataExpedicao.getTime())) {
+                console.log('Data inválida. Por favor, insira uma data válida.')
+            }
+        } while (isNaN(dataExpedicao.getTime()))
         let cpf = new Documento(numero, TipoDocumento.CPF, dataExpedicao)
         this.cliente.Documentos.push(cpf)
     }
--- src/ts/processos/documentos/cadastroPassaporte.ts
+++ src/ts/processos/documentos/cadastroPassaporte.ts
@@ -2,6 +2,7 @@ import Processo from "../../abstracoes/processo";
 import { TipoDocumento } from "../../enumeracoes/TipoDocumento";
 import Cliente from "../../modelos/cliente";
 import Documento from "../../modelos/documento";
+import Armazem from "../../dominio/armazem";
 
 export default class CadastroPassaporte extends Processo {
     private cliente: Cliente
@@ -11,8 +12,30 @@ export default class CadastroPassaporte extends Processo {
     }
 
     processar(): void {
-        let numero = this.entrada.receberTexto('Qual o número do documento?')
-        let dataExpedicao = this.entrada.receberData('Qual a data de expedição do documento?')
+        let numero: string
+        let passaporteExistente: boolean
+
+        do {
+            numero = this.entrada.receberTexto('Qual o número do documento?')
+            passaporteExistente = Armazem.InstanciaUnica.Clientes.some(cliente =>
+                cliente.Documentos.some(doc => doc.Tipo === TipoDocumento.Passaporte && doc.Numero === numero)
+            )
+            if (passaporteExistente) {
+                console.log('Passaporte já existe. Por favor, insira um passaporte diferente.')
+            } else if (isNaN(Number(numero))) {
+                console.log('Número de passaporte inválido. Por favor, insira um número composto apenas por números.')
+                passaporteExistente = true
+            }
+        } while (passaporteExistente || !numero)
+
+        let dataExpedicao: Date
+        do {
+            dataExpedicao = this.entrada.receberData('Qual a data de expedição do documento?')
+            if (isNaN(dataExpedicao.getTime())) {
+                console.log('Data inválida. Por favor, insira uma data válida.')
+            }
+        } while (isNaN(dataExpedicao.getTime()))
+
         let pass = new Documento(numero, TipoDocumento.Passaporte, dataExpedicao)
         this.cliente.Documentos.push(pass)
     }
