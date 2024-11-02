13b1fb909f35f594b3490647ca7bfde31d146579
Juan Garcia Soares <juan.garcia.soares@gmail.com>
2024-11-01 23:59:30 -0300

    implementando o footer no sistema

---
+++ front/src/componentes/footer.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import logo from '../public/logo.png';
+
+export default function Footer() {
+    return (
+        <footer className="bg-black text-light py-4">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-4 mb-4">
+                        <img className="logo mb-2" src={logo} alt="PetLovers" />
+                        <p>© 2024 PetLovers. Todos os direitos reservados.</p>
+                    </div>
+                    <div className="col-md-4 mb-4">
+                        <h5>Acesso Rápido</h5>
+                        <ul className="list-unstyled">
+                            <li><a className="text-light" href="/clientes">Clientes</a></li>
+                            <li><a className="text-light" href="/cadastrocliente">Cadastrar Cliente</a></li>
+                            <li><a className="text-light" href="/vinculo">Hospedagem</a></li>
+                            <li><a className="text-light" href="/listagens">Listagens</a></li>
+                        </ul>
+                    </div>
+                    <div className="col-md-4 mb-4">
+                        <h5>Entre em Contato</h5>
+                        <p>Email: support@petlovers.com</p>
+                        <p>Phone: (123) 456-7890</p>
+                        <p>Address: 123 Pet Street, Pet City, PC 12345</p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
+}
